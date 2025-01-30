3dd47523f8c6fe0fcc61875c84af077eb5fbc309
gulceabaci <gulceabaci_66@hotmail.com>
2025-01-30 19:48:37 +0300

    data moved to separate file

---
+++ react-vite/src/data.js
@@ -0,0 +1,30 @@
+export const items = [
+    {
+      "image" : "../public/img/Iphone16Pro.jpg",
+      "title" : "Iphone 16 Pro",
+      "decription" : "The iPhone 16 Pro is a smartphone designed, developed, and marketed by Apple Inc. It is the fourteenth generation of the iPhone, succeeding the iPhone 15 Pro and iPhone 15 Pro Max, and was announced on September 14, 2022, with pre-orders beginning on September 17, 2022, and official release on September 24, 2022.",
+      "price" : 110000,
+      is_active: true
+    },
+    {
+      "image" : "../public/img/Iphone11.jpeg",
+      "title" : "Iphone 11",
+      "decription" : "The iPhone 11 is a smartphone designed, developed, and marketed by Apple Inc. It is the thirteenth generation of the iPhone, succeeding the iPhone XS and XS Max. It was unveiled on September 10, 2019, alongside the higher-end iPhone 11 Pro and iPhone 11 Pro Max.",
+      "price" : 60000,
+      is_active: true
+    },
+    {
+      "image" : "../public/img/HuwaiP40.jpeg",
+      "title" : "Huwai P40",
+      "decription" : "The Huawei P40 is a line of Android-based smartphones developed by Huawei as part of the P series. It was announced in March 2020 alongside the Huawei P40 Pro and Huawei P40 Pro+, and was Huawei's first flagship series to not include Google Mobile Services.",
+      "price" : 50000,
+      is_active: true
+    },
+    {
+      "image" : "../public/img/SamsungS23.jpeg",
+      "title" : "Samsung S23",
+      "decription" : "The Samsung Galaxy S23 is a line of Android-based smartphones manufactured, released and marketed by Samsung Electronics. The Galaxy S23 series is a successor to the Galaxy S22 series and was unveiled on 9 February 2023.",
+      "price" : 100000,
+      is_active: true
+    },
+  ];  
