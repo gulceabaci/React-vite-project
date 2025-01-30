bf371981577922e0ab509d652ab6b45d2f42e49b
gulceabaci <gulceabaci_66@hotmail.com>
2025-01-30 21:16:29 +0300

    description

--- react-vite/src/components/Product.jsx
+++ react-vite/src/components/Product.jsx
@@ -7,7 +7,7 @@ export default function Product({ productObj }) {
         <img className="card-img-top p-2 p-md-3 border-bottom" src={productObj.image} alt="mobile phone" />
         <div className="card-body">
           <h2 className="card-title">{productObj.title}</h2>
-          <p className="card-text">{productObj.decription}</p>
+          <p className="card-text">{productObj.description}</p>
           <span className="badge text-bg-success">{productObj.price} TL</span>
         </div>
       </div>
